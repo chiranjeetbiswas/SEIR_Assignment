e55a6b12e802ae74fd23a514162502c0456a3cff
Chiranjeet Biswas <chiranjeetbiswas403@gmail.com>
2026-02-23 23:15:05 +0530

    make frequency dictionary

--- assign1.py
+++ assign1.py
@@ -22,4 +22,18 @@ def get_body_title_link(response):
     return cleanbody,title,links
 
 
+def word_count(str):
+    word_list=str.split()
+    word_frequency={}
+    stopwords=["the", "is", "am", "are" ,"not" ,"a" ,"very"]
+    for word in word_list:
+        if word in stopwords:
+            continue
+        if word in word_frequency:
+            word_frequency[word]+=1
+        else:
+            word_frequency[word]=1
+    return word_frequency
+
+
 
